60ec39a59b00ab763981536c6c9bca4ee7ee0239
Akshitha-1023 <akshithaindian@gmail.com>
2025-07-07 17:54:55 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:8000/'
+        target:"https://shop-inventory-management-mern-backend.onrender.com"
       }
     }
   },
